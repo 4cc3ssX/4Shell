06c1f54becfb8dc9b36b54471957a1c4ef32c180
4cc3ss Flick <57473799+4cc3ssflick@users.noreply.github.com>
2020-04-27 22:49:01 +0630

    Some bug fixed

--- client.py
+++ client.py
@@ -1,9 +1,10 @@
 import socket
 import sys
+import os
  
 def Main():
         host = '127.0.0.1'
-        port = 5000
+        port = 4723
          
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
@@ -11,21 +12,30 @@ def Main():
         except ConnectionRefusedError:
                 print("[\033[033m ERROR \033[00m] Cannot connect to shell. \n\t[\033[033m Err \033[00m: Connection refused! ]")
                 sys.exit()
-        print("[\033[032m OK \033[00m]Shell has been connected with %s" % host)
+        print("[\033[032m OK \033[00m] Shell has been connected with %s" % host)
         try:
-                shell = input("%s~$\033[033m" % host)
-                
-                while shell != 'q':
-                        s.send(shell.encode())
-
-                        data = s.recv(4096).decode()
-                        print('\t')
-                        print ('\033[00m\033[030m'+data+'\033[00m')
-                        shell = input("\033[00m%s~$\033[033m" % host)
+            s.send("whoami".encode())
+            user = s.recv(1024).decode()
+            if user == "root":
+                    print("[ \033[031mROOT\033[00m ] Shell is connected as root!")
+                    euid = 0
+            else:
+                    euid = 1
+            prompt(euid,host)
+            while not shell:
+                prompt(euid,host)
+            while shell != 'exit':
+                s.send(shell.encode())
+                data = s.recv(4096).decode()
+                if not data: break
+                print ('\033[00m\033[032m%s\033[00m' % (data))
+                prompt(euid,host)
         except KeyboardInterrupt:
                 print("\n\033[00m[\033[033m ERROR \033[00m] Exiting the shell. \n\t[\033[033m Err \033[00m: Keyboard Interrupt! ]")
                 sys.exit()
         s.close()
- 
+def prompt(euid, host):
+    global shell
+    shell = input("\033[00m%s~%s\033[033m" % (host, "#" if euid==0 else "$"))
 if __name__ == '__main__':
     Main()
