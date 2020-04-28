463ba59bd23f05d497d745039bcaadb72557c029
4cc3ss Flick <57473799+4cc3ssflick@users.noreply.github.com>
2020-04-28 12:23:10 +0630

    Some bug fixed

--- server.py
+++ server.py
@@ -2,7 +2,8 @@ import socket
 import subprocess
 import sys
 import os
- 
+from colorama import Fore,Back
+
 def Main():
     host = "0.0.0.0"
     port = 4723
@@ -12,45 +13,62 @@ def Main():
     try:
         s.bind((host,port))
         s.listen(5)
-        print("[\033[032m OK \033[00m] Binding server at %s:%s on %s" % (host, port, os.uname()[1]))
+        resp(0,"Binding server at %s:%s on %s" % (host,port,os.uname()[1]))
     except PermissionError:
-        print("\033[00m[\033[033m ERROR \033[00m] Exiting the shell. \n\t[\033[033m Err \033[00m: Permission Denied! ]")
+        resp(1,"Permission Denied!")
         sys.exit()
     except OSError as e:
         if e.args[0] == 98:
-            print("\033[00m[\033[033m ERROR \033[00m] Exiting the shell. \n\t[\033[033m Err \033[00m: Address already in use! ]")
+            resp(1,"Address already in use!")
             sys.exit()
     conn, addr = s.accept()
-    print ("[\033[032m OK \033[00m] 4Shell connected with: %s" % str(addr[0]))
+    resp(0, "4Shell connected with: %s" % (str(addr[0])))
     while True:
         try:
             data = conn.recv(4096).decode()
         except KeyboardInterrupt:
-            print("\n\033[00m[\033[033m ERROR \033[00m] Exiting the shell. \n\t[\033[033m Err \033[00m: Keyboard Interrupt! ]")
+            resp(1,"Keyboard Interrupt!")
             sys.exit()
         if not data: break
         data = str(data)
         response = shell(data)
         try:
-            conn.send(response.strip().encode())
+            conn.send(response.encode())
         except ConnectionResetError:
-            print("\033[00m[\033[031m ERROR \033[00m] Connection Closed. Try Again!")
+            resp(1,"Connection Closed. Try Again!")
             sys.exit()
+def resp(typo,message):
+    if typo == 1:
+        print(f"{Fore.RESET}[ {Fore.RED}ERROR{Fore.RESET} ] Exiting the shell. \n\t[\033[033m Err \033[00m: {message} ]")
+    elif typo == 0:
+        print(f"{Fore.RESET}[ {Fore.GREEN}OK{Fore.RESET} ] {message}")
 def shell(command):
     command = command.rstrip()
+    c_com = command[0:2]
     try:
-        if command[0:2] == 'cd':
+        if c_com == 'cd':
             try:
                 os.chdir(command[3:])
+                run = "cd: directory changed: %s" % command[3:]
             except FileNotFoundError:
-                print("No such file or directory: %s" % command[3:])
+                run = "cd: No such file or directory: %s" % command[3:]
+        elif c_com == 'pwd':
             run = os.getcwd()
         else:
-            run = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True, encoding='utf-8')
+            output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True, encoding='utf-8')
+            if not output:
+                run = "%s: command excuted" % c_com
+            else:
+                run = output
     except subprocess.CalledProcessError:
-        run = "\033[00m[\033[031m-\033[00m] Command not found"
-    except:
-        run = "\033[00m[\033[031m-\033[00m] Failed to execute command"
+        run = "%s: command not found" % c_com
     return run
 if __name__ == '__main__':
-    Main()
+    if sys.platform == "linux":
+        Main()
+    else:
+        win = input("Supported platform is out of range. Continue?[Y/n] ")
+        if win.lower() == "y":
+            Main()
+        else:
+            sys.exit(0)
